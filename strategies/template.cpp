9d5daffe3ea34429080338f5f9c9cb5b1e0c55f9
Sunny Cheung <hyc55@cam.ac.uk>
2025-06-29 11:57:28 +0800

    add back template.cpp

---
+++ strategies/template.cpp
@@ -0,0 +1,51 @@
+#include <random>
+
+#include "../lib/types.h"
+
+using namespace wallgo;
+
+#ifdef RED
+
+namespace red {
+
+class PlayerImpl : public Player {
+   private:
+    // Local variables
+
+   public:
+    void init(PlayerColor player, std::shared_ptr<const Game> game, int seed) override {}
+
+    Position place(const std::vector<Position>& valid_positions) override {}
+
+    Move move(const std::vector<Move>& valid_moves) override {}
+};
+
+// Provide a factory function for the engine to use
+std::unique_ptr<wallgo::Player> get() { return std::unique_ptr<wallgo::Player>(new PlayerImpl()); }
+
+}  // namespace red
+
+#endif  // RED
+
+#ifdef BLUE
+
+namespace blue {
+
+class PlayerImpl : public Player {
+   private:
+    // Local variables
+
+   public:
+    void init(PlayerColor player, std::shared_ptr<const Game> game, int seed) override {}
+
+    Position place(const std::vector<Position>& valid_positions) override {}
+
+    Move move(const std::vector<Move>& valid_moves) override {}
+};
+
+// Provide a factory function for the engine to use
+std::unique_ptr<wallgo::Player> get() { return std::unique_ptr<wallgo::Player>(new PlayerImpl()); }
+
+}  // namespace blue
+
+#endif  // BLUE
