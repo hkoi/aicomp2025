6e693ffe0ff3d4d654ecb78371d422bbed637c5c
Sunny Cheung <53392449+PunnyBunny@users.noreply.github.com>
2025-06-30 08:16:24 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-g++ strategies/impl.cpp -DRED  -c -o strategies/red.o  -Ilib 
-g++ strategies/impl.cpp -DBLUE -c -o strategies/blue.o -Ilib 
-g++ lib/grader.cpp strategies/red.o strategies/blue.o lib/game_controller.cpp lib/types.cpp -o exec.exe
+g++ strategies/impl.cpp -std=c++20 -Wno-unused-result -DRED  -c -o strategies/red.o  -Ilib 
+g++ strategies/impl.cpp -std=c++20 -Wno-unused-result -DBLUE -c -o strategies/blue.o -Ilib 
+g++ lib/grader.cpp strategies/red.o strategies/blue.o lib/game_controller.cpp lib/types.cpp -std=c++20 -Wno-unused-result -o exec.exe
