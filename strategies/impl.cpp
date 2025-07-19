9b8492db31501c1600cf0d43eb99f6e078c4bab7
Ethen Yuen <ethening@gmail.com>
2025-07-19 16:41:47 +0800

    add some more template for the template impl

--- strategies/impl.cpp
+++ strategies/impl.cpp
@@ -12,13 +12,22 @@ namespace {
 class PlayerImpl : public Player {
    private:
     // Local variables
+    std::mt19937 rng_;
+    std::shared_ptr<const Game> game_;
 
    public:
-    void init(PlayerColor player, std::shared_ptr<const Game> game, int seed) override {}
+    void init(PlayerColor player, std::shared_ptr<const Game> game, int seed) override {
+		rng_ = std::mt19937(seed);
+		game_ = game;
+	}
 
-    Position place(PieceId pieceId, const std::vector<Position>& valid_positions) override {}
+    Position place(PieceId pieceId, const std::vector<Position>& valid_positions) override {
+        // Implement your placement logic here
+    }
 
-    Move move(const std::vector<Move>& valid_moves) override {}
+    Move move(const std::vector<Move>& valid_moves) override {
+        // Implement your move logic here
+    }
 };
 }  // namespace
 
